fdee3c84ed73ec973c4133b92a217286c38d0382
wz <44930227+wenlzhang@users.noreply.github.com>
2025-03-23 20:10:03 +0100

    Fine tune settings tab

--- src/settingsTab.ts
+++ src/settingsTab.ts
@@ -13,16 +13,9 @@ export class TagIndexSettingTab extends PluginSettingTab {
         const { containerEl } = this;
         containerEl.empty();
 
-        containerEl.createEl("h2", { text: "Tag Index Settings" });
-
-        containerEl.createEl("p", {
-            text: "Tag Index allows you to create a separate list of important tags for quick access.",
-            cls: "setting-item-description",
-        });
-
         new Setting(containerEl)
             .setName("Add new tags to top")
-            .setDesc("When enabled, new tags will be added to the top of the tag list. When disabled, they will be added to the bottom.")
+            .setDesc("When enabled, new tags will be added to the top of the tag list; otherwise, they will be added to the bottom.")
             .addToggle(toggle => toggle
                 .setValue(this.plugin.settings.addTagsToTop)
                 .onChange(async (value) => {
